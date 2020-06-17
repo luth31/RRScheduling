2257048fcefc2d512179904378dc41e550c82b3c
Paul Munteanu <munteanu.paul.c@gmail.com>
2020-06-17 05:37:49 +0200

    Add menu and menu helper functions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RRScheduling
 {
     class Program
     {
+        static Dictionary<int,(String, Func<int>)> MenuTable;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MenuTable = new Dictionary<int,(String, Func<int>)>();
+            BuildMenuTable();
+            ShowMenu();
+        }
+        static int Test1() {
+            Console.WriteLine("test1");
+            return 0;
+        }
+    
+        static int Test2() {
+            Console.WriteLine("test2");
+            return 0;
+        }
+        static void AddMenuItem(String str, Func<int> fn) {
+            MenuTable.Add(MenuTable.Count, (str, fn));
+        }
+        static void BuildMenuTable() {
+            AddMenuItem("Test", Test1);
+            AddMenuItem("Test2", Test2);
+            AddMenuItem("Exit", () => {
+                Environment.Exit(0);
+                return 0; });
+        }
+        static void ShowMenu() {
+            Console.WriteLine("Menu:");
+            foreach (var entry in MenuTable) {
+                Console.WriteLine("{0}. {1}", entry.Key, entry.Value.Item1);
+            }
+            ReadSelection();
+        }
+        static void ReadSelection() {
+            Console.Write("Selector: ");
+            string input = Console.ReadLine();
+            int selector = -1;
+            try {
+                Int32.TryParse(input, out selector);
+            }
+            catch {
+                InvalidSelection();
+            }
+            if (MenuTable.ContainsKey(selector)) {
+                MenuTable[selector].Item2();
+                ShowMenu();
+            }
+            else
+                InvalidSelection();
+        }
+        static void InvalidSelection() {
+            Console.WriteLine("Invalid selection!");
+            ReadSelection();
         }
     }
 }
