af909fe04986238893e09c5a12bd03b7eeda6361
Paul Munteanu <munteanu.paul.c@gmail.com>
2020-06-17 08:15:15 +0200

    Add taskList and order tasks by StartTime and insertion order

--- Scheduler.cs
+++ Scheduler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace RRScheduling {
     class Scheduler {
@@ -6,8 +7,19 @@ namespace RRScheduling {
             if (Quantum < 1)
                 Quantum = 1;
             this.Quantum = Quantum;
+            taskList = new List<Task>();
         }
-
+        public void AddTask(Task task) {
+            // Keep list ordered by StartTime and by insertion order
+            for (int i = taskList.Count; i > 0; --i) {
+                if (task.StartTime >= taskList[i-1].StartTime) {
+                    taskList.Insert(i, task);
+                    return;
+                }
+            }
+            taskList.Insert(0, task);
+        }
         int Quantum = 0;
+        List<Task> taskList;
     }
 }
